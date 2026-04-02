a2cb1b9e04806d08402a59bc7539143132e3b226
Ubuntu <marguerite27@hanyang.ac.kr>
2026-04-02 14:26:07 +0000

    CSHP v2-2

--- scripts/set_ministack.sh
+++ scripts/set_ministack.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# 1. 기본 환경 변수 설정
+# 환경 변수 설정
 export AWS_ACCESS_KEY_ID=test
 export AWS_SECRET_ACCESS_KEY=test
 export AWS_DEFAULT_REGION=us-east-1
@@ -8,32 +8,34 @@ ALIAS_AWS="aws --endpoint-url=http://localhost:4566"
 
 echo "[*] Initializing LocalStack Resources..."
 
-# 1. 기존에 떠 있는 모든 stress-test 관련 함수 삭제 (청소)
-awslocal lambda delete-function --function-name stress-test-shm 2>/dev/null
-awslocal lambda delete-function --function-name stress-test-shm-container 2>/dev/null
+# 0. 기존에 떠 있는 모든 test 관련 함수 삭제
+# awslocal lambda delete-function --function-name stress-test-lambda 2>/dev/null
+# awslocal lambda delete-function --function-name stress-test-shm 2>/dev/null
+# awslocal lambda delete-function --function-name stress-test-shm-container 2>/dev/null
 
-# 2. 함수 생성 (이름 딱 하나로 고정: stress-test-shm)
+# 1. 함수 생성
 awslocal lambda create-function \
-    --function-name stress-test-shm \
+    --function-name stress-test-lambda \
     --package-type Image \
-    --code ImageUri=my-shm-lambda:latest \
+    --code ImageUri=my-lambda:latest \
     --role arn:aws:iam::000000000000:role/lambda-ex \
     --memory-size 1024 \
     --timeout 30
 
-# 3. S3 권한 부여 (이게 빠지면 400 에러 납니다)
+# 2. S3 권한 부여
 awslocal lambda add-permission \
-    --function-name stress-test-shm \
+    --function-name stress-test-lambda \
     --statement-id s3-cross-account \
     --action "lambda:InvokeFunction" \
     --principal s3.amazonaws.com \
     --source-arn arn:aws:s3:::my-bucket
 
-# 4. S3 -> Lambda 트리거 연결 (핵심!)
-# S3 버킷에 파일이 생성(Put)될 때 람다를 호출하도록 설정합니다.
+# 3. S3 -> Lambda 트리거 연결
+# S3 버킷에 파일이 생성(Put)될 때 Lambda를 호출하도록 설정
 echo "[*] Configuring S3 Event Trigger..."
+
 awslocal lambda add-permission \
-    --function-name stress-test-shm \
+    --function-name stress-test-lambda \
     --statement-id s3-trigger \
     --action "lambda:InvokeFunction" \
     --principal s3.amazonaws.com \
@@ -44,7 +46,7 @@ $ALIAS_AWS s3api put-bucket-notification-configuration \
     --notification-configuration '{
         "LambdaFunctionConfigurations": [
             {
-                "LambdaFunctionArn": "arn:aws:lambda:us-east-1:000000000000:function:stress-test-shm",
+                "LambdaFunctionArn": "arn:aws:lambda:us-east-1:000000000000:function:stress-test-lambda",
                 "Events": ["s3:ObjectCreated:*"]
             }
         ]
