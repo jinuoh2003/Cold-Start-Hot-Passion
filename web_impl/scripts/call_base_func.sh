49b6f6e6cee2ee3b96b41940faa11ca05f05066e
jinuoh2003 <45008959+jinuoh2003@users.noreply.github.com>
2026-04-03 10:42:54 +0900

    Update call_base_func.sh

--- web_impl/scripts/call_base_func.sh
+++ web_impl/scripts/call_base_func.sh
@@ -1,7 +1,5 @@
 awslocal lambda invoke \
     --function-name stress-test-baseline \
-    --payload fileb:///home/ubuntu/Cold-Start-Hot-Passion/dummy_data/payload_100mb.json \
+    --payload "fileb://$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")/../.." && pwd)/dummy_data/payload_100mb.json" \
     --cli-binary-format raw-in-base64-out \
     response_baseline.json
-
-
